e12066fab2a3c7a23c4926222ec4bcf04c47c101
rimbaajati <audysa040802@gmail.com>
2025-11-22 18:21:40 +0700

    Upgrade Transaction

--- app/Filament/Resources/Transactions/Schemas/TransactionForm.php
+++ app/Filament/Resources/Transactions/Schemas/TransactionForm.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Transactions\Schemas;
 
 use App\Models\Departement;
+use App\Models\Transaction;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
@@ -11,53 +12,47 @@ use Filament\Schemas\Schema;
 
 class TransactionForm
 {
+    protected static ?string $model = Transaction::class;
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     public static function configure(Schema $schema): Schema
     {
-        return $schema
-            ->components([
-                
+        return $schema->components([
                 // CODE
                 TextInput::make('code')
                     ->required()
                     ->default(fn() => 'TRX-' . mt_rand(100000, 999999)),
-
-                // USER ID (Catatan: TextInput tidak support ->relationship, tapi saya tetap biarkan karena
-                // kamu bilang "jangan ubah fungsi/algoritma")
+                // USER ID 
                 Select::make('user_id')
                     ->required()
                     ->label('User')
                     ->relationship('user', 'name'),
-
                 // PAYMENT STATUS
                 TextInput::make('payment_status')
                     ->readOnly()
                     ->default('PENDING'),
-
-                // FIELDSET
+                // DEPARTEMENT  SELECT AND COST DISPLAY
                 Section::make("Departement")
-                ->schema([
-                    Select::make('departement_id')
-                        ->required()
-                        ->label("Departement Name & Semester")
-                        ->options(Departement::query()->get()->mapWithKeys(function ($departement) {
-                            return [
-                                $departement->id => $departement->name . ' - Semester ' . $departement->semester
-                            ];
-                        })->toArray())
-                        ->reactive()
-                        ->afterStateUpdated(function ($state, callable $set) {
-                            if ($departement = Departement::find($state)) {
-                                $set('departement_cost', $departement->cost);
-                            } else {
-                                $set('departement_cost', null);
-                            }
-                        }),
+                    ->schema([
+                        Select::make('departement_id')
+                            ->required()
+                            ->label("Departement Name & Semester")
+                            ->options(
+                                Departement::query()
+                                ->get()
+                                ->mapWithKeys(fn ($d) => [
+                                    $d->id => "{$d->name} - Semester {$d->semester}"
+                                ])
+                            )
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set) {
+                                $departement = Departement::find($state);
+                                $set('departement_cost', $departement?->cost);
+                                }),
+                        TextInput::make('departement_cost')
+                            ->label('Cost')
+                            ->readOnly(),
+                    ])
 
-                    TextInput::make('departement_cost')
-                        ->label('Cost')
-                        ->readOnly(),
-                ])
-
-                        ]);
+                            ]);
+        }
     }
-}
